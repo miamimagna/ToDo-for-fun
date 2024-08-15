1099339c3d3e5d5bf8d3ab75ac23b1dffa0d8879
miamimagna <miamimagna@gmail.com>
2024-08-15 19:26:13 +0530

    skeleton-project templates

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const cookieParser= require('cookie-parser');
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(logger('dev'));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.listen(port, () => {
+    console.log('Listening to the port: ', port);
+})
+
